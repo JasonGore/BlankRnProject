1aac616bb59b0ad97f4a9daaef4c47b7ae0ec01b
Jason Gore <jagore@microsoft.com>
2022-04-15 10:48:03 -0700

    Repack integration.

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
+import React from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+const styles = {};
+
+const RNContainer = ({ scores }) => {
+    const contents =
+      scores && scores.map
+        ? scores.map((score) => (
+            <Text key={score.name}>
+              {score.name}:{score.value}
+              {'\n'}
+            </Text>
+          ))
+        : 'scores not available';
+    return (
+      <View style={styles.container}>
+        <Text style={styles.highScoresTitle}>2048 High Scores!</Text>
+        <Text style={styles.scores}>{contents}</Text>
+      </View>
+    );
+  };
+  
+
 AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerComponent('RNContainer', () => RNContainer);
